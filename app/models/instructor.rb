91e8fe1fc8d22fa5c86fb18224bd6417de2ecfdd
kaidornel <kai_dornel@hotmail.com>
2020-03-15 17:24:42 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,77 @@
+require 'pry'
 class Instructor
 
+    attr_reader :name
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pass_student(student_name, test_name)
+        testinfo = BoatingTest.all.select do |info|
+            info.student.first_name == student_name && info.bt_name == test_name
+        end
+        
+        if testinfo
+            testinfo.bt_status = "passed"
+        else
+            newtest = Boatingtest.new(student_name, test_name, "passed", self)
+        end
+    end
+
+    def fail_student(student_name, test_name)
+        testinfo = BoatingTest.all.select do |info|
+            info.student.first_name == student_name && info.bt_name == test_name
+        end
+        if testinfo
+            testinfo.bt_status = "failed"
+        else
+            newtest = Boatingtest.new(student_name, test_name, "failed", self)
+        end
+    end
+
 end
+#initialize with a name
+#Instructor.all should return all instructors
+
+#Instructor#pass_student should take in a student instance and test name.
+# If there is a BoatingTest whose name and student match the values passed in, 
+# this method should update the status of that BoatingTest to 'passed'. If there 
+# is no matching test, this method should create a test with the student, that 
+# boat test name, and the status 'passed'. Either way, it should return the BoatingTest instance.
+
+#Instructor#fail_student should take in a student instance and test name. Like #pass_student, it 
+#should try to find a matching BoatingTest and update its status to 'failed'. If it cannot find an 
+#existing BoatingTest, it should create one with the name, the matching student, and the status 'failed'.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #     tests = BoatingTest.all.select { |boating_test|
+    #         boating_test.bt_name == test_name && boating_test.student.first_name == student_name    
+    # }
+    #     if tests == []
+    #         stud = Student.find_student(student_name)
+    #         new = BoatingTest.new(stud, test_name, "passed", self)
+    #         return new
+    #     else
+    #         tests.map{|test| test.bt_status}
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,55 @@
 class Student
+    attr_accessor :first_name, :num_passed
+    @@all = []
+    def initialize(first_name, num_passed)
+        @first_name = first_name
+        @num_passed = num_passed
+        @@all << self
 
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test (student, bt_name, bt_status, instructor)
+        Boatingtest.new(student, bt_name, bt_status, instructor)
+    end
+
+    def self.find_student(students_name)
+        self.all.select do |student|
+            student.first_name == students_name
+            binding.pry
+        end
+    end
+
+    def grade_percentage
+        studentSelector = BoatingTest.all.select do |data|
+            data.student.first_name == self.first_name
+        end
+        totalTests = studentSelector.length
+        passed_tests = studentSelector.select do |data|
+            data.bt_status == "passed"
+        end
+        numPassed = passed_tests.length.to_f
+        percentage = numPassed/totalTests * 100
+        binding.pry
+    end
 end
+
+#should initialize with first_name
+
+# should include return all of the student instances
+
+# Student#add_boating_test should initialize a new boating 
+# test with a Student (Object), a boating test name (String), 
+# a boating test status (String), and an Instructor (Object)
+
+# Student.find_student will take in a first name and output the 
+# student (Object) with that name
+
+# Student#grade_percentage should return the percentage of 
+# tests that the student has passed, a Float (so if a student 
+# has passed 3 / 9 tests that they've taken, this method should 
+# return the Float 33.33)
+
