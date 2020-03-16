30d0a3f9571e5a3374639b1d2add64e18c69265c
richardbobo1 <richard.bobo@gmail.com>
2020-03-15 22:52:44 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,33 @@
+
 class Instructor
+
+    def initialize(name)
+        @name = name
+    end 
+
+
+    def pass_student(student, test)
+
+        BoatingTest.all.select |test| if student == student && test == test 
+        status = "pass" 
+        else new_test = BoatingTest.New(student, test, "Pass", self)
+        end 
+
+  
+
+
+    end 
+
+    def fail_student(student, test)
+
+        BoatingTest.all.select |test| if student == student && test == test 
+        status = "pass" 
+        else new_test = BoatingTest.New(student, test, "Fail", self)
+        end 
+
+    end 
+
+
+
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,24 @@
 class Student
 
+    @@all = [] 
+
+    def initialize(first_name) 
+        @first_name = first_name 
+
+        @@all << self 
+
+    end 
+
+    def self.all
+        @@all 
+    end 
+
+    def add_boating_test(self, test_name, status, instructor)
+
+        new_test = BoatingTest.new(self, test_name, status, instructor)
+
+    end 
+
+
+    
 end
